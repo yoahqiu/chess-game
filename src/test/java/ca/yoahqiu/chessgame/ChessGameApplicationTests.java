54a77af1e2044049fe258df7e7282d8b2d04de56
yoahqiu <yoah.qiu@gmail.com>
2022-03-02 17:14:50 -0500

    Initial commit

---
+++ src/test/java/ca/yoahqiu/chessgame/ChessGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.yoahqiu.chessgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
